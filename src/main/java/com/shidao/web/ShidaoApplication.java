35efc4b30a3438b6eaedf9f104d43ad5781a7098
liwe69 <lwenzhi@sino-cr.org>
2018-03-28 14:19:15 +0800

    完善数据库配置（可连通） 增加interceptor

--- src/main/java/com/shidao/web/ShidaoApplication.java
+++ src/main/java/com/shidao/web/ShidaoApplication.java
@@ -1,11 +1,13 @@
 package com.shidao.web;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
+@MapperScan("com.shidao.web.dao")
 public class ShidaoApplication extends SpringBootServletInitializer {
 
 	@Override
--- src/main/java/com/shidao/web/controller/TestController.java
+++ src/main/java/com/shidao/web/controller/TestController.java
@@ -1,13 +1,31 @@
 package com.shidao.web.controller;
 
+import java.lang.reflect.InvocationTargetException;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
+
+import org.apache.commons.beanutils.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.shidao.web.dao.UserMapper;
+import com.shidao.web.model.User;
+import com.shidao.web.model.UserExample;
+import com.shidao.web.model.UserExample.Criteria;
+
 @RestController
 public class TestController {
-
+	@Autowired
+	UserMapper um;
     @GetMapping("/helloworld")
-    public String helloworld() {
-        return "helloworld3";
+    public Map helloworld() throws IllegalAccessException, InvocationTargetException, NoSuchMethodException {
+    	UserExample example=new UserExample();
+    	Criteria criteria = example.createCriteria();
+		List<User> ulist=um.selectByExample(example);
+		User user=ulist.get(0);
+		Map map=BeanUtils.describe(user);
+        return map;
     }
 }

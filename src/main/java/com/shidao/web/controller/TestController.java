95d4e86c84b4a528ce1be714e9bc0f0115a8c806
liwe69 <lwenzhi@sino-cr.org>
2018-03-29 17:45:27 +0800

    数据库连通

--- src/main/java/com/shidao/web/controller/TestController.java
+++ src/main/java/com/shidao/web/controller/TestController.java
@@ -13,14 +13,19 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.shidao.web.dao.DictTypeMapper;
+import com.shidao.web.model.DictType;
+
 
 
 @RestController
 public class TestController {
-	
+	@Autowired
+	DictTypeMapper dict;
     @GetMapping("/helloworld")
     public Map helloworld() throws IllegalAccessException, InvocationTargetException, NoSuchMethodException {
-    	
-        return null;
+    	DictType dt=dict.selectByPrimaryKey(1);
+    	return BeanUtils.describe(dt);
+       
     }
 }

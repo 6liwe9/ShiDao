f182c9270fa38df797fbca42bd850ff066babd26
liwe69 <lwenzhi@sino-cr.org>
2018-05-22 09:26:36 +0800

    修改url 配置

--- src/main/java/com/shidao/web/commons/RequestInterceptor.java
+++ src/main/java/com/shidao/web/commons/RequestInterceptor.java
@@ -1,5 +1,9 @@
 package com.shidao.web.commons;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
@@ -7,15 +11,33 @@ import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
 public class RequestInterceptor implements HandlerInterceptor {
+	static {
+		Properties properties = new Properties();
+		  // 使用ClassLoader加载properties配置文件生成对应的输入流
+		    InputStream in = RequestInterceptor.class.getClassLoader().getResourceAsStream("url.properties");
+		   // 使用properties对象加载输入流
+	     try {
+			properties.load(in);
+			 String url=properties.getProperty("allowurl");
+			 allowUrls=url;
+			 
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		     //获取key对应的value值
+	    
 
-	String[] allowUrls= {"/user","Login","Register","Forget","/js","/image","/images","/fonts","/css","/libs"};
+	}
+	static String allowUrls;
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
 			throws Exception {
 		HttpSession session =request.getSession();   
         UserToken user =(UserToken) session.getAttribute("userToken");
         String url =request.getRequestURL().toString();  
-        for(String s:allowUrls) {
+        String[] urls=allowUrls.split(";");
+        for(String s:urls) {
         	if(url.contains(s))
         		return true;
         }
